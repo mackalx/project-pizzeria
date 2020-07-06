6698ef203e6447e9178dfbb054fde77acc028d0d
Maciej Kalita <maciej.kalita@gmail.com>
2020-07-06 23:30:32 +0200

    Add initData method Edit initMenu by adding loop iterating over thisApp.data.products object to pass arguments to constructor

--- src/js/script.js
+++ src/js/script.js
@@ -62,10 +62,21 @@
 
   const app = {
     initMenu: function(){
-      const testProduct = new Product();
-      console.log('testProduct:', testProduct);
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
-    
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -74,6 +85,7 @@
       console.log('settings:', settings);
       console.log('templates:', templates);
 
+      thisApp.initData();
       thisApp.initMenu();
     },
   };
