9d130a74f6b19e5bf0b089745a9071aa7bcc5f91
Maciej Kalita <maciej.kalita@gmail.com>
2020-08-18 20:08:44 +0200

    Add fetching data from the API after JSON modifications

--- src/js/script.js
+++ src/js/script.js
@@ -77,6 +77,11 @@
       defaultDeliveryFee: 20,
     },
     // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
   };
   
   const templates = {
@@ -507,14 +512,30 @@
       // console.log('thisApp.data:', thisApp.data);
 
       for(let productData in thisApp.data.products){
-        new Product(productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]); // the only place where we used product key was here - now instead of a key, we use id
       }
     },
 
     initData: function(){
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+      
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url) // using the fetch function we send a query to the provided endpoint address
+        .then(function(rawResponse){
+          return rawResponse.json(); // convert the received response from JSON to an array
+        })
+        .then(function(parsedResponse){
+          console.log('parsedResponse:', parsedResponse); // after receiving the converted parsedResponse response, we display it in the console
+          /* [DONE] save parsedResponse as thisApp.data.products */
+          thisApp.data.products = parsedResponse;
+          /* [DONE] execute initMenu method */
+          thisApp.initMenu();
+        });
+
+      console.log('thisApp.data:', JSON.stringify(thisApp.data));
     },
 
     initCart: function(){ // method initiating cart instance - we give it the cart's wrapper
@@ -533,7 +554,6 @@
       // console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
     },
   };
