d0bb609881530c450da94b50cddc6b62e81b9de6
Maciej Kalita <maciej.kalita@gmail.com>
2020-08-18 14:13:49 +0200

    Add CartProduct.remove and CartProduct.initActions - CartProduct now sends remove event

--- src/js/script.js
+++ src/js/script.js
@@ -411,6 +411,8 @@
         }
       }
     }
+
+    
   }
 
   class CartProduct{
@@ -426,6 +428,7 @@
 
       thisCartProduct.getElements(element);
       thisCartProduct.initAmountWidget();
+      thisCartProduct.initActions();
 
       // console.log('new CartProduct', thisCartProduct);
       // console.log('productData', menuProduct);
@@ -451,6 +454,34 @@
         thisCartProduct.amount = thisCartProduct.amountWidget.value;
         thisCartProduct.price = thisCartProduct.priceSingle * thisCartProduct.amount;
         thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+      });
+    }
+
+    remove(){
+      const thisCartProduct = this;
+
+      const event = new CustomEvent('remove',{
+        bubbles: true,
+        detail: {
+          cartProduct: thisCartProduct,
+        },
+      });
+
+      thisCartProduct.dom.wrapper.dispatchEvent(event);
+
+      console.log('remove', event);
+    }
+
+    initActions(){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom.edit.addEventListener('click', function (event){
+        event.preventDefault();
+      });
+
+      thisCartProduct.dom.remove.addEventListener('click', function (event){
+        event.preventDefault();
+        thisCartProduct.remove();
       });
     }
   }
