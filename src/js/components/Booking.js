3093cd892b5af2fdec83bc5450d8de96a20a6640
Maciej Kalita <maciej.kalita@gmail.com>
2020-09-01 22:12:50 +0200

    Add Booking.getData Edit app.json event and booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,22 +1,82 @@
-import {select, templates} from '../settings.js';
+import {select, templates, settings} from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 
 class Booking{
-  constructor(element){
+  constructor(wrapper){
     const thisBooking = this;
 
-    thisBooking.render(element);
+    thisBooking.render(wrapper);
     thisBooking.initWidgets();
+    thisBooking.getData();
   }
 
-  render(element){
+  getData(){
+    const thisBooking = this;
+
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    // console.log('getData params', params);
+
+    const urls = {
+      booking:        settings.db.url + '/' + settings.db.booking 
+                                      + '?' + params.booking.join('&'),
+      eventsCurrent:  settings.db.url + '/' + settings.db.event   
+                                      + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:   settings.db.url + '/' + settings.db.event   
+                                      + '?' + params.eventsRepeat.join('&'),
+    };
+
+    // console.log('getData urls', urls);
+
+    Promise.all([ // what does it do exactly & date problems (event 1 & 5) in app.json booking????
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function(allResponses){
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        console.log(bookings);
+        console.log(eventsCurrent);
+        console.log(eventsRepeat);
+      });
+  }
+
+  render(wrapper){
     const thisBooking = this;
     const generatedHTML = templates.bookingWidget();
 
     thisBooking.dom = {};
-    thisBooking.dom.wrapper = element;
+    thisBooking.dom.wrapper = wrapper;
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
 
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
